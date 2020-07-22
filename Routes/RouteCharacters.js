e7fe81a24174ce7302cf145cbc11632a038be9ab
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-22 12:39:30 +0200

    refactor(routecharacters): get one id

--- Routes/RouteCharacters.js
+++ Routes/RouteCharacters.js
@@ -13,4 +13,21 @@ router.get('/', (req, res) => {
   })
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  connection.query('SELECT * from Characters WHERE id = ?',id, (err, results) => {
+    if (err) {
+      return (
+        res.status(500).json({ message: 'Internal server error' })
+      )
+    }
+    if (results.length === 0) {
+      return (
+        res.status(404).json({ message: 'PurchasesOrders ID not found' })
+      )
+    }
+    res.json(results[0]) 
+  })
+});
+
 module.exports = router; 
