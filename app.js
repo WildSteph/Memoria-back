b6c0d81c0819d7f95044193a8005429804b5d45a
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-22 13:43:24 +0200

    refacort(app) : add cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 8000;
 const indexRouter = require('./Routes/Index');
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
